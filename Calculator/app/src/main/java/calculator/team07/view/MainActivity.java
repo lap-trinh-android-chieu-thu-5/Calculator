b1cada0e97a4500b39857c82db1730b9b8352cd1
DESKTOP-GIUI4CL\Hai Nguyen <hainguyenkt98@gmail.com>
2019-03-16 01:31:06 +0700

    done - expression fragment

--- Calculator/app/src/main/java/calculator/team07/view/MainActivity.java
+++ Calculator/app/src/main/java/calculator/team07/view/MainActivity.java
@@ -1,9 +1,12 @@
 package calculator.team07.view;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import calculator.team07.R;
+import calculator.team07.fragment.ExpressionFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        ExpressionFragment frm = new ExpressionFragment();
+        fragmentTransaction.add(R.id.top, frm);
+        fragmentTransaction.commit();
     }
 }
